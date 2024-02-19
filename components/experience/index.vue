06144886cc8c96dc61d0f5f05d5ae96f2f0452c4
Mustafa Muhammad <mustafa.m257@proton.me>
2024-02-19 15:08:12 -0500

    Add layout with max width to about

--- components/experience/index.vue
+++ components/experience/index.vue
@@ -1,24 +1,32 @@
 <template>
-  <div class="w-full gap-5 py-16 mx-auto max-w-7xl lg:px-8">
-    <h1
-      class="text-3xl font-bold tracking-wide text-zinc-800 sm:text-5xl dark:text-zinc-100"
-    >
-      Work
-    </h1>
-    <div class="flex flex-col max-w-3xl gap-5 py-10">
-      <ExperienceCard
-        v-for="experience in work"
-        :item="experience"
-        class="px-4 py-8 bg-blue-500"
-      />
+  <div
+    class="flex flex-row items-center justify-between w-full gap-5 py-16 mx-auto lg:px-8 max-w-7xl"
+  >
+    <div class="">
+      <h1
+        class="text-3xl font-bold tracking-wide text-zinc-800 sm:text-5xl dark:text-zinc-100"
+      >
+        Work
+      </h1>
+      <div class="flex flex-col max-w-3xl gap-5 py-10">
+        <ExperienceCard
+          v-for="experience in work"
+          :item="experience"
+          class="px-4 py-8 bg-blue-500"
+        />
+      </div>
+      <h1
+        class="text-3xl font-bold tracking-wide text-zinc-800 sm:text-5xl dark:text-zinc-100"
+      >
+        Education
+      </h1>
+      <div class="flex flex-col max-w-3xl gap-5 py-10">
+        <ExperienceCard :item="education" class="px-4 py-8 bg-blue-500" />
+      </div>
     </div>
-    <h1
-      class="text-3xl font-bold tracking-wide text-zinc-800 sm:text-5xl dark:text-zinc-100"
-    >
-      Education
-    </h1>
-    <div class="flex flex-col max-w-3xl gap-5 py-10">
-      <ExperienceCard :item="education" class="px-4 py-8 bg-blue-500" />
+
+    <div class="flex flex-col gap-4">
+      <Icon name="i-logos:nuxt" v-for="i in 10" class="w-10 h-10" />
     </div>
   </div>
 </template>
