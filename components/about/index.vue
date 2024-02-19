554e0b2f46967097edac49deb078be5d0c6d877d
Mustafa Muhammad <mustafa.m257@proton.me>
2024-02-18 20:21:08 -0500

    Structured about section, next to finalize data and UI

--- components/about/index.vue
+++ components/about/index.vue
@@ -25,18 +25,29 @@
         </div>
       </div>
       <div class="lg:pl-20">
-        <ul role="list" class="text-white">
-          <a href="#"> Follow on X </a>
-          <a href="#" class="mt-4"> Follow on Instagram </a>
-          <a href="#" class="mt-4"> Follow on GitHub </a>
-          <a href="#" class="mt-4"> Follow on LinkedIn </a>
-          <a
-            href="mailto:mustafa.m257@proton.me"
-            icon="{MailIcon}"
-            class="pt-8 mt-8 border-t border-zinc-100 dark:border-zinc-700/40"
+        <ul role="list" class="flex flex-col gap-5 text-white">
+          <li class="flex gap-5">
+            <Icon name="i-line-md:instagram" class="w-6 h-6" />
+            <p>Follow on Instagram!</p>
+          </li>
+          <li class="flex gap-5">
+            <Icon name="i-line-md:linkedin" class="w-6 h-6" />
+            <p>Follow on LinkedIn!</p>
+          </li>
+          <li class="flex gap-5">
+            <Icon name="i-line-md:github" class="w-6 h-6" />
+            <p>Follow on Github!</p>
+          </li>
+          <li
+            class="flex gap-5 pt-8 mt-8 border-t border-zinc-100 dark:border-zinc-700/40"
           >
-            mustafa.m257@proton.me
-          </a>
+            <Icon name="i-line-md:email" class="w-6 h-6" />
+            <p>mustafa.m257@proton.me</p>
+          </li>
+          <li class="flex gap-5">
+            <Icon name="i-line-md:phone" class="w-6 h-6" />
+            <p>+1 (347) 382-2775</p>
+          </li>
         </ul>
       </div>
     </div>
